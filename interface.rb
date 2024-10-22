02c6f4cb5d17c4820508f3d7844bd1ef8fe5ad04
Roberto Barros <roberto.f.barros@gmail.com>
2024-10-22 11:25:20 -0300

    comments

--- interface.rb
+++ interface.rb
@@ -1,3 +1,5 @@
+gifts = ["bike", "t-shirt", "doll", "ted bear", "iphone 16", "chocolate"]
+
 def list(gifts)
   gifts.each_with_index do |gift,index|
     puts "#{index+1} - #{gift}"
@@ -6,29 +8,28 @@ end
 
 # Mensagem de boas vindas
 puts "Bem vindo a nossa lista de Natal!"
-gifts = ["bike", "t-shirt", "doll", "ted bear", "iphone 16", "chocolate"]
+
 # LOOP
 loop do
   # Perguntar qual opção deseja [list|add|delete|quit]
   puts "Qual opção voce deseja executar?[list|add|delete|quit]"
   action = gets.chomp
-  # Executar (fake) de acordo com o escolhido
+  # Executar de acordo com o escolhido
   case action
-  when "list"
-    list(gifts)
+  when "list" then list(gifts)
   when "add"
     puts "Adcionando um presente na lista"
   when "delete"
     puts "deletando presente da lista"
   when "quit"
+    # Sair se for quit
     break
   else
     puts "ação inválida"
   end
-  # Sair se for quit
 
+# FIM DO LOOP
 end
-# END
 
 # Mensagem de goodbye
 puts "Obrigado por realizar a sua lista de natal!"
