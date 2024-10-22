bc2a5f23cebfa7499e907d5585fc838e7f075aa1
Roberto Barros <roberto.f.barros@gmail.com>
2024-10-22 10:49:15 -0300

    pseudocode

---
+++ interface.rb
@@ -0,0 +1,13 @@
+# Mensagem de boas vindas
+
+# LOOP
+
+  # Perguntar qual opção deseja [list|add|delete|quit]
+
+  # Executar (fake) de acordo com o escolhido
+
+  # Sair se for quit
+
+# END
+
+# Mensagem de goodbye
