4fe9e0cd7a3b0f8dafb03c71958aaf5005f3ec82
Maira Almeida <mairasenna@gmail.com>
2024-10-22 11:47:47 -0300

    list add delete

--- interface.rb
+++ interface.rb
@@ -1,8 +1,30 @@
-gifts = ["bike", "t-shirt", "doll", "ted bear", "iphone 16", "chocolate"]
+gifts = []
 
 def list(gifts)
   gifts.each_with_index do |gift,index|
     puts "#{index+1} - #{gift}"
+  end
+end
+
+def add(gifts)
+  # Perguntar qual o presente a ser adicionado a lista
+  puts "Qual o presente você quer adicionar a lista?"
+  add_gift = gets.chomp
+  # adicionar o presente no array de gifts
+  gifts << add_gift
+end
+
+def delete(gifts)
+  # Listar (exibir) todos os presentes
+  list(gifts)
+  # Perguntar qual o número do presente a ser deletado da lista
+  puts "Qual o número do presente você quer remover da lista?"
+  index_gift = gets.chomp.to_i - 1
+  if index_gift >= 0 && index_gift < gifts.count
+    # Remover do nosso array de gifts pelo index
+    gifts.delete_at(index_gift)
+  else
+    puts "Número inválido!"
   end
 end
 
@@ -17,10 +39,8 @@ loop do
   # Executar de acordo com o escolhido
   case action
   when "list" then list(gifts)
-  when "add"
-    puts "Adcionando um presente na lista"
-  when "delete"
-    puts "deletando presente da lista"
+  when "add" then add(gifts)
+  when "delete" then delete(gifts)
   when "quit"
     # Sair se for quit
     break
